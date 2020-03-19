c45e4598e0c1132d3bb0a0dfa70b81915b4f57a6
Manish Thapa <manish.thapa@utilidex.com>
2020-03-19 16:36:54 +0530

    Commiting the changes made to login page

--- src/test/java/com/utilidex/testcases/LoginPageTC.java
+++ src/test/java/com/utilidex/testcases/LoginPageTC.java
@@ -10,8 +10,11 @@ public class LoginPageTC extends BaseClass{
 	@Test
 	public void loginAlpha() {
 	
+		//Enter the email address and password
 		driver.findElement(By.xpath(OR.getProperty("email"))).sendKeys("manish.thapa@utilidex.com");
 		driver.findElement(By.xpath(OR.getProperty("passward"))).sendKeys("07Oct@2019");
+		
+		//Click on the sign button 
 		driver.findElement(By.xpath(OR.getProperty("signinBtn"))).click();
 	}
 }
